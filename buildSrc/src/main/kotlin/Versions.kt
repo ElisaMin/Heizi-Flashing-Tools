dd6e33e7f7649e107f9cb0645851ad9dbe25f8b7
Heizi <xm.zhongjiaxin@gmail.com>
2022-04-12 19:04:07 +0800

    updating version : 0.0.5 compose 1.1.0-alpha -> 1.1.1 kotlin 1.6.10 -> 1.6.20 coroutine 1.6.0 -> 1.6.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,15 +4,15 @@ package me.heizi.gradle
 
 object Versions {
     const val jna = "5.9.0"
-    const val HFT = "0.0.4"
-    const val compose = "1.1.0-alpha04"
+    const val HFT = "0.0.5"
+    const val compose = "1.1.1"
     const val decompose = "0.5.1"
-    const val kotlin = "1.6.10"
+    const val kotlin = "1.6.20"
     const val slf4j = "1.7.35"
 }
 object Libs {
     const val M3 = "org.jetbrains.compose.material3:material3:${Versions.compose}"
-    const val Coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0"
+    const val Coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1"
     const val DecomposeX = "com.arkivanov.decompose:extensions-compose-jetbrains:${Versions.decompose}"
     const val Decompose = "com.arkivanov.decompose:decompose-jvm:${Versions.decompose}"
     object SLF4J {
