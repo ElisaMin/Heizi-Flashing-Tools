62383f52bc008a3b01c9afc05c6ec4f2c625f6ae
Heizi <xm.zhongjiaxin@gmail.com>
2023-03-27 06:00:51 +0800

    重写算了 #20

--- libs/compose-ext-core/src/main/kotlin/me/heizi/kotlinx/compose/desktop/core/MonetUtils.kt
+++ libs/compose-ext-core/src/main/kotlin/me/heizi/kotlinx/compose/desktop/core/MonetUtils.kt
@@ -9,28 +9,32 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.colorspace.Rgb
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toPixelMap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.singleWindowApplication
 import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.flow.*
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 import kotlin.math.max
 import kotlin.math.sqrt
+import kotlin.random.Random
+import kotlin.system.exitProcess
 
 suspend fun main() {
     // read buffered image "C:\Users\xmzho\Desktop\BK5KO3O@)FHVPFD$6Z5]]}0.gif" and covert it as a sequence of color
     val colors = ImageIO.read(File("C:\\Users\\xmzho\\Desktop\\BK5KO3O@)FHVPFD$6Z5]]}0.gif")).let {
-        sequence {
-            for (x in 0 until it.width)
-                for (y in 0 until it.height)
-                    yield(Color(it.getRGB(x, y)))
-        }
+//        sequence {
+//            for (x in 0 until it.width)
+//                for (y in 0 until it.height)
+//                    yield(Color(it.getRGB(x, y)))
+//        }
+        Pixels(it)
     }
     // run k-means algorithm k=5
-    val kMeans = kMeansNew(120,colors.map { it.toRGB() })
+    val kMeans = colors.kMeansLasts(5)
     kMeans.forEach {
         println(it)
     }
@@ -45,6 +49,196 @@ suspend fun main() {
     }
 }
 
+abstract class Pixels:Iterator<RGB> {
+    abstract fun pixel(x:Int,y:Int):RGB
+    abstract fun size():Pair<Int,Int>
+    private var index = 0
+    // override iterator
+    override fun hasNext(): Boolean = index < size().first*size().second
+    override fun next(): RGB = pixel(index/size().second,index%size().second).also { index++ }
+
+    companion object {
+//        operator fun invoke(colors:Sequence<RGB>):Pixels = object : Pixels() {
+//            override fun pixel(x: Int, y: Int): RGB = colors.elementAt(x*size().second+y)
+//            override fun size(): Pair<Int, Int> = Pair(colors.count()/size().second,size().second)
+//        }
+//        operator fun invoke(colors:List<RGB>):Pixels = object : Pixels() {
+//            override fun pixel(x: Int, y: Int): RGB = colors[x*size().second+y]
+//            override fun size(): Pair<Int, Int> = Pair(colors.size/size().second,size().second)
+//        }
+        // jetpack compose image
+        operator fun invoke(image: ImageBitmap):Pixels = object : Pixels() {
+            override fun pixel(x: Int, y: Int): RGB = image.toPixelMap()[x,y].toRGB()
+            override fun size(): Pair<Int, Int> = Pair(image.width,image.height)
+        }
+        operator fun invoke(image: BufferedImage):Pixels = object : Pixels() {
+            override fun pixel(x: Int, y: Int): RGB = Color(image.getRGB(x,y)).toRGB()
+            override fun size(): Pair<Int, Int> = Pair(image.width,image.height)
+        }
+    }
+}
+fun distance(a: RGB, b: RGB): Float {
+    val rd = a.red - b.red
+    val gd = a.green - b.green
+    val bd = a.blue - b.blue
+    return sqrt(rd * rd + gd * gd + bd * bd)
+}
+
+fun distances(a: RGB, b: RGB): Float =
+    sqrt((a.red - b.red) * (a.red - b.red) +
+            (a.green - b.green) * (a.green - b.green) +
+            (a.blue - b.blue) * (a.blue - b.blue))
+
+fun Pixels.kMeansLasts(k: Int): List<RGB> {
+    val pixels = this
+    // Initialize centroids by randomly selecting k pixels
+    var centroids = List(k) { pixel(Random.nextInt(size().first),Random.nextInt(size().second)) }.toMutableList()
+    var clusters: MutableMap<RGB, MutableList<RGB>>
+    var closestCentroid: RGB
+    var closestDist: Float
+    var dist: Float
+    var moved: Boolean
+    var numIterations = 0
+
+    do {
+        numIterations++
+        clusters = mutableMapOf()
+        moved = false
+        for (p in pixels) {
+            closestCentroid = centroids[0]
+            closestDist = distances(p, closestCentroid)
+
+            for (i in 1 until k) {
+                dist = distance(p, centroids[i])
+                if (dist < closestDist) {
+                    closestCentroid = centroids[i]
+                    closestDist = dist
+                }
+            }
+
+            if (!clusters.containsKey(closestCentroid)) {
+                clusters[closestCentroid] = mutableListOf(p)
+                moved = true
+            } else if (!clusters[closestCentroid]!!.contains(p)) {
+                clusters[closestCentroid]!!.add(p)
+                moved = true
+            }
+        }
+
+        // Recompute centroids
+        for (i in 0 until k) {
+            if (!clusters.containsKey(centroids[i])) {
+                centroids = MutableList(k) { pixel(Random.nextInt(size().first),Random.nextInt(size().second)) }
+                break
+            }
+            var red = 0f
+            var green = 0f
+            var blue = 0f
+            for (pixel in clusters[centroids[i]]!!) {
+                red += pixel.red
+                green += pixel.green
+                blue += pixel.blue
+            }
+            centroids[i] = RGB(
+                red / clusters[centroids[i]]!!.size,
+                green / clusters[centroids[i]]!!.size,
+                blue / clusters[centroids[i]]!!.size
+            )
+        }
+    } while (numIterations < 100 && moved)
+
+    return centroids.sortedWith { a, b ->
+        when(val aIsBetter = a.toHSB() isBetterThen b.toHSB()){
+            (aIsBetter == null) -> 0
+            aIsBetter -> -1
+            else -> 1
+        }
+    }
+}
+
+fun Pixels.kMeansLast(k:Int):List<RGB> {
+    val (width,height) = size()
+    // init centroids mutable list as random pixels
+    val centroids = (0 until k).map { pixel(Random.nextInt(width-1),Random.nextInt(height-1)) }.toMutableList()
+    // init clusters mutable list as empty list
+    val clusters = mutableMapOf<RGB,MutableList<RGB>>()
+    while (true) {
+        // clear clusters
+        clusters.clear()
+        // init clusters
+        centroids.forEach { clusters[it] = mutableListOf() }
+        // for each pixel
+        forEach { pixel ->
+            // find the nearest centroid
+            val nearest = centroids.minByOrNull { distance(it,pixel) }!!
+            // add the pixel to the cluster
+            clusters[nearest]!!.add(pixel)
+        }
+        // init new centroids
+        val newCentroids = mutableListOf<RGB>()
+        // rgb as double
+
+        // for each cluster
+        clusters.forEach { (centroid, cluster) ->
+            println(centroid)
+            // init new centroid
+            var red = 0.0
+            var green = 0.0
+            var blue = 0.0
+            var size = 0
+            // for each pixel in the cluster
+            cluster.forEach { pixel ->
+                // add the pixel to the new centroid
+                red += pixel.red
+                green += pixel.green
+                blue += pixel.blue
+                size++
+            }
+            // all not zero
+            if (0.0 !in listOf(red,green,blue)) {
+                red /= size
+                green /= size
+                blue /= size
+            }
+
+
+            if (red.isNaN() || green.isNaN() || blue.isNaN()) {
+                println("NaN")
+                // output rgb
+                println(red)
+                println(green)
+                println(blue)
+                exitProcess(-1)
+            }
+
+            println(red)
+            // divide the new centroid by the size of the cluster as float
+            val newCentroid = RGB(red.toFloat(),green.toFloat(),blue.toFloat())
+            // add the new centroid to the new centroids
+            newCentroids.add(newCentroid)
+            println(newCentroid)
+        }
+        // if the new centroids are the same as the old centroids
+        if (centroids == newCentroids) {
+            // return the new centroids
+            return newCentroids
+        }
+        // set the centroids to the new centroids
+        centroids.clear()
+        centroids.addAll(newCentroids)
+    }
+
+}
+
+@Suppress("NOTHING_TO_INLINE")
+private inline operator fun RGB.plus(pixel: RGB): RGB {
+    return Triple(first + pixel.first,second + pixel.second,third + pixel.third)
+}
+// div by float
+@Suppress("NOTHING_TO_INLINE")
+private inline operator fun RGB.div(div: Int): RGB {
+    return Triple(first / div,second / div,third / div)
+}
 
 
 //suspend fun Flow<RGB>.kMeans(k:Int,size: Int = -1): Flow<RGB> = coroutineScope {
