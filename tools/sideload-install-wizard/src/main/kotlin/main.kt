fe495532fa89a983b9b950e0e954b77ef82778f5
Heizi <xm.zhongjiaxin@gmail.com>
2023-03-31 12:30:16 +0800

    AST Done #20

--- tools/sideload-install-wizard/src/main/kotlin/main.kt
+++ tools/sideload-install-wizard/src/main/kotlin/main.kt
@@ -1,27 +1,39 @@
 @file:JvmName("Main")
 package me.heizi.flashing_tool.sideloader
 
-import androidx.compose.animation.core.updateTransition
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.toPainter
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
-import kotlinx.coroutines.*
+import com.mayakapps.compose.windowstyler.WindowBackdrop
+import com.mayakapps.compose.windowstyler.windows.WindowsWindowStyleManager
+import dev.kdrag0n.colorkt.rgb.Srgb
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Unconfined
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.takeWhile
-import me.heizi.compose.ext.monet.common.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import me.heizi.compose.ext.monet.common.Monet
+import me.heizi.compose.ext.monet.common.MonetWindow
+import me.heizi.compose.ext.monet.common.getWindowFrame
 import me.heizi.flashing_tool.adb.ADB
 import me.heizi.flashing_tool.adb.ADBDevice
 import me.heizi.flashing_tool.sideloader.screens.invoke
+import me.heizi.kotlinx.compose.desktop.core.setAllBackground
 import me.heizi.kotlinx.logger.debug
 import me.heizi.kotlinx.logger.println
+import java.awt.Window
 import java.io.File
 import java.net.URL
 import javax.imageio.ImageIO
@@ -31,7 +43,7 @@ import javax.swing.JPanel
 import kotlin.system.exitProcess
 
 
-val context: MutableStateFlow<Context> =
+val context: MutableStateFlow<Context?> =
     MutableStateFlow(Context.Ready)
 
 val colors:ColorScheme
@@ -44,6 +56,7 @@ val ColorScheme.current get() = this
 var contextReady by mutableStateOf(false)
 
 fun main(args: Array<String>) {
+    println("官网: dl.lge.fun 或 tools.lge.fun\nQQ群: 549674080")
     val frame = JFrame("正在加载中").apply {
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         val label = JLabel("正在加载中")
@@ -61,72 +74,118 @@ fun main(args: Array<String>) {
         }
     }
     frame.isVisible = true
-    runBlocking {
+    Context.scope.launch {
         init_state.emit("初始化Compose")
+        WindowsWindowStyleManager(frame).apply {
+            MonetWindow.systemColor?.let {seek ->
+                dev.kdrag0n.monet.theme.ColorScheme.Dynamic[Srgb(seek)]
+            }?.run {
+                isDarkTheme = MonetWindow.isDark
+                if (isDarkTheme) materialDark() else materialLight()
+            }?.run {
+                (surface as Srgb).toRgb8().let {
+                    frame.setAllBackground(java.awt.Color(it))
+                }
+                frameStyle = getWindowFrame()
+            }
+        }
     }
     application {
-        init_state.value = "初始化完成"
-        LaunchedEffect("init the icon") {
-
-            init_state.value = "初始化图标"
-            init_state.value = "初始化图标"
-            Resources.iconASTUgly
+        init_state.value = "初始化Compose完成"
+        var icon: Painter? by remember {
+            mutableStateOf(null)
         }
-
-        if (!contextReady) StanWindow {
-            Context.Ready()
-            kotlin.io.println("官网: dl.lge.fun 或 tools.lge.fun\nQQ群: 549674080")
-            requireAndStuck(args.size==1) {
-                "请输入正确的文件地址"
-            }
-            LaunchedEffect("init the icon") {
-                init_state.value = "初始化图标"
-                Resources.iconASTUgly
-            }
-            LaunchedEffect(frame) {
-                init(args,frame)
-                MonetWindow.isDark
-                MonetWindow.systemColor
-            }
-            DisposableEffect(frame) {
-                onDispose {
-                    frame.isVisible = false
-                    init_state.value = null
-                    frame.dispose()
+        init_state.value = "正在加载图标"
+        LaunchedEffect("icon") {
+            icon = Resources.iconASTUgly
+            init_state.value = "图标加载完成"
+        }
+        init_state.value = "初始化Compose完成"
+        init_state.value = "正在加载Application"
+        var the_window:Window? = null
+        if (the_window == null&&icon==null&&!contextReady) Window(
+            onCloseRequest = this::exitApplication,
+            visible = !contextReady,
+            title = "",
+            icon = BitmapPainter(ImageBitmap(1,1,)),
+            state = WindowState(position = WindowPosition(Alignment.Center), size = DpSize(width = 420.dp, height = 580.dp)),
+        ) {
+            LaunchedEffect(window) {
+                WindowsWindowStyleManager(window).let {
+                    it.backdropType = WindowBackdrop.Tabbed
+                    it.isDarkTheme = MonetWindow.isDark
                 }
             }
-        } else StanWindow(true) {
-            val currentContext by context.collectAsState()
-            "Context".println("current",currentContext::class.simpleName)
-            Monet {
-                val seek = remember(currentContext) {
-                    (currentContext as? SingleFileContext)?.color?.let { c->
-                        "TESTING".println("color",c)
-                        this.color = c
-                        "TESTING".println("color","window",color)
+            var text by remember { mutableStateOf("正在加载中...") }
+            Context.Ready(text)
+            if (icon==null) {
+                LaunchedEffect(icon) {
+                    init_state.value = "正在加载窗口"
+                }
+                LaunchedEffect(frame) {
+                    Context.scope.launch {
+                        init(args,frame)
                     }
                 }
-                LaunchedEffect(seek) {
-                    updateColorBySystemAccent()
+                DisposableEffect(frame) {
+                    onDispose {
+                        frame.isVisible = false
+                        init_state.value = null
+                        frame.dispose()
+                    }
                 }
-                app(currentContext)
+
+            } else if (!contextReady) {
+                val currentContext by context.collectAsState()
+                text = "正在加载文件"
+                if (currentContext == null) {
+                    icon = null
+                    text = "文件加载失败，\n或许下一个版本\n会加入拖拽功能。"
+                } else Monet {  }
+            } else Monet {
+                the_window = window
+            }
+        }
+        Window(
+            visible = contextReady && icon!=null ,
+            onCloseRequest = {exitApplication()},
+            title = "",
+            icon = icon,
+            state = WindowState(position = WindowPosition(Alignment.Center), size = DpSize(width = 420.dp, height = 580.dp)),
+        ) {
+            LaunchedEffect(window) {
+                WindowsWindowStyleManager(window)
+                while (the_window == null) delay(10)
+                the_window!!.isVisible = false
+                the_window!!.dispose()
+                the_window = null
+            }
+            if (icon!=null) {
+                ReadyToGo()
             }
         }
     }
 }
-
 @Composable
-inline fun ApplicationScope.StanWindow(
-    icon:Boolean = false,
-    crossinline content: @Composable FrameWindowScope.() -> Unit
-){
-    Window(
-        onCloseRequest = this::exitApplication,
-        title = "",
-        icon = if (icon) Resources.iconASTUgly else null,
-        state = WindowState(position = WindowPosition(Alignment.Center), size = DpSize(width = 420.dp, height = 580.dp)),
-        content = { content() }
-    )
+fun FrameWindowScope.ReadyToGo() = Monet {
+    val currentContext by context.collectAsState()
+    "Context".println("current",currentContext?.let { it::class.simpleName })
+    val seek = remember(currentContext) {
+        (currentContext as? SingleFileContext)?.color?.also { c->
+            "TESTING".println("color",c)
+            this.color = c
+            "TESTING".println("color","window",color)
+        }
+    }
+    LaunchedEffect(seek) {
+        updateColorBySystemAccent()
+        windowStyler
+    }
+    currentContext?.let {
+        app(it)
+    }?: kotlin.run {
+        Context.Ready("错误！")
+    }
 }
 @Composable
 fun FrameWindowScope.app(currentContext:Context) {
@@ -209,7 +268,7 @@ fun FrameWindowScope.app(currentContext:Context) {
         }
         else -> {
             init_state.value = "初始化中"
-            Context.Ready()
+            Context.Ready("错误")
         }
     }
 
@@ -260,14 +319,9 @@ suspend fun init(args: Array<String>,frame: JFrame) = Context.scope.launch {
 //            colorJob.cancel()
             adbJob.cancel()
             init_state.emit("文件解析失败")
-            init_state.emit("文件解析失败")
-            init_state.emit("文件解析失败")
             init_state.emit(null)
-            delay(1000)
+            context.emit(null)
             frame.isVisible = false
-            requireAndStuck(false) {
-                "文件解析失败"
-            }
         }
     }
 }
